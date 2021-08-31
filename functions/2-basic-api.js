fb68dad09a8018a16629afce771b04bb32ce080b
Serge-boro <serhiiborodin83@gmail.com>
2021-08-30 20:23:03 -0700

    allow

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,7 @@ const items = require('../assets/data')
 
 exports.handler = async (event, context) => {
   return {
+    header: { 'Access-Control-Allow-Origin': '*'}
     statusCode: 200,
     body: JSON.stringify(items),
   }
